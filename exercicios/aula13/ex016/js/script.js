d95d8c1c9ece51151a6c97d068af86926a679c41
ArthurNK18 <arthur123_NK@outlook.com>
2025-02-18 19:23:45 -0300

    Update ex JS

--- exercicios/aula13/ex016/js/script.js
+++ exercicios/aula13/ex016/js/script.js
@@ -1,1 +1,22 @@
-alert('ola')
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12) {
+        //Bom dia!
+        img.src = 'fotomanha.png'
+        document.body.style.background = '#e2cd9f'
+    }else if (hora >= 12 && hora <= 18) {
+        //Bom Tarde!!
+        img.src = 'fototarde.png'
+        document.body.style.background = '#b9846f'
+    } else {
+        //Boa noite!!
+        img.src = 'fotonoite.png'
+        document.body.style.background = '#515154'
+    }
+}
+
+
